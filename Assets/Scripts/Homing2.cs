a208560df535941015ce456bfe505c4a810f4d46
MovingGrass <cocobear.dmt@gmail.com>
2024-11-24 23:21:36 +0700

    homing bug fix
    fix bullet doesnt damage enemy and doesnt disappear on collission

---
+++ Assets/Scripts/Homing2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homing2: MonoBehaviour, IPowerUp
+{
+    // Start is called before the first frame update
+    public GameObject bulletPrefab;
+    public Transform shootingPoint2;
+    public float fireRate = 0.2f;
+    private float nextFireTime = 0f;
+     public float Duration => 10f;
+
+    public void Activate()
+    {
+        enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && Time.time >= nextFireTime)
+        {
+            ShootBullet();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    void ShootBullet()
+    {
+        Instantiate(bulletPrefab, shootingPoint2.position, shootingPoint2.rotation);
+    }
+}
--- Assets/Scripts/HomingMissiles.cs
+++ Assets/Scripts/HomingMissiles.cs
@@ -59,7 +59,7 @@ public class HomingMissiles : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         // Handle impact effects here (explosion, damage, etc)
-        if (collision.collider.CompareTag("Player2"))
+        if (collision.collider.CompareTag("Player1"))
         {
             // Try to get the HealthPlayer1 component from the object
             HealthPlayer1 player1Health = collision.collider.GetComponent<HealthPlayer1>();
@@ -70,5 +70,18 @@ public class HomingMissiles : MonoBehaviour
                 player1Health.TakeDamagePlayer1(bulletDamage);
             }
         }
+        // Check if the object has the tag "Player2"
+        else if (collision.collider.CompareTag("Player2"))
+        {
+            // Try to get the HealthPlayer2 component from the object
+            HealthPlayer2 player2Health = collision.collider.GetComponent<HealthPlayer2>();
+
+            if (player2Health != null)
+            {
+                // Call TakeDamagePlayer1 method in HealthPlayer2 script and pass in the damage
+                player2Health.TakeDamagePlayer2(bulletDamage);
+            }
+        }
+        Destroy(gameObject);
     }
 }
