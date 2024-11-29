4e368fea485ea143604e977a3d1200a82669a99b
Juli-10 <abang10jul@gmail.com>
2024-11-29 14:42:37 +0700

    Fix_animation_bugs

--- Assets/Scripts/Player 2 Movement.cs
+++ Assets/Scripts/Player 2 Movement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player2Movement : MonoBehaviour
 {
-   public float moveSpeed = 5f;
+    public float moveSpeed = 5f;
+    public Animator animator;
     private Rigidbody rb;
 
     void Start()
@@ -19,6 +20,8 @@ public class Player2Movement : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        animator.SetFloat("Horizontal", moveHorizontal);
+        animator.SetFloat("Vertical", moveVertical);
 
         // Rotate the player to face the direction of movement
         if (movement != Vector3.zero)
